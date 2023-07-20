2ff878d89cca321384a4ab78a66a90cce807e8ed
Marcella Wigg <mwigg@predictiveindex.com>
2023-07-20 16:50:36 -0400

    Test blocking of fake Stripe API key.

--- server/webapp.py
+++ server/webapp.py
@@ -6,7 +6,7 @@ from flask import Flask
 
 ROOT = os.path.dirname(os.path.abspath(os.path.dirname(__file__)))
 TEMPLATES = os.path.join(ROOT, 'templates')
-
+STRIPESECRET = 'sk_test_4eC39HqLyjWDarjtT1zdp7dc'
 flaskapp = Flask("BookStore", template_folder=TEMPLATES)
 flaskapp.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
